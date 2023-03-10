8863b95b4a0b575dc1e6dd94f290776095bd8293
kalhollywood <kal.hollywood@gmail.com>
2023-03-10 10:31:00 +0000

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+@section('content')
+
+  <article>
+    <h1>{!! $post->title !!}</h1>
+
+            <p>
+                    By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name}}</a> in <a href="/categories/{{ $post->category->slug}}">{{$post->category->name}}</a>
+                </p>
+    
+    <div>
+        <p>
+            {!! $post->body !!}
+        </p>
+
+
+    </div>
+
+
+    </article>
+
+    <a href="/">Go Back</a>
+
+@endsection
