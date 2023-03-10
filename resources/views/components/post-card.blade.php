43bb6115c62a857968c6426cf38eea6828309023
kalhollywood <kal.hollywood@gmail.com>
2023-03-10 15:57:29 +0000

    completed the filter for categories

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -4,7 +4,7 @@
     {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5">
         <div>
-            <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="mt-8 flex flex-col justify-between">
@@ -26,22 +26,15 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
+            <div class="text-sm mt-4 space-y-4">
 
-                <p class="mt-4">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur.
-                </p>
+                {!! $post->excerpt !!}
+
             </div>
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">Lary Laracore</h5>
                         <h6>Mascot at Laracasts</h6>
