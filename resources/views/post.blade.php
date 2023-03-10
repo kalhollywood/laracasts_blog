df1012168360faf5729af75c563aa4fc21940901
kalhollywood <kal.hollywood@gmail.com>
2023-03-10 11:38:29 +0000

    created components for front page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,25 +1,24 @@
 @extends('layout')
 
 @section('content')
+    <article>
+        <h1>{!! $post->title !!}</h1>
 
-  <article>
-    <h1>{!! $post->title !!}</h1>
-
-            <p>
-                    By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name}}</a> in <a href="/categories/{{ $post->category->slug}}">{{$post->category->name}}</a>
-                </p>
-    
-    <div>
         <p>
-            {!! $post->body !!}
+            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
+        <div>
+            <p>
+                {!! $post->body !!}
+            </p>
 
-    </div>
+
+        </div>
 
 
     </article>
 
     <a href="/">Go Back</a>
-
 @endsection
