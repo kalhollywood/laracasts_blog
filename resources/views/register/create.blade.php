95a2df8489d8fd7ca9877dc25dafdaadc6f3d8b0
kalhollywood <kal.hollywood@gmail.com>
2023-04-07 13:09:42 +0100

    added login and logout with validation

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,7 +2,7 @@
 
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
+            <h1 class="text-center font-bold text-xl">Register</h1>
             <form action="/register" method="POST" class="mt-10">
                 @csrf
 
